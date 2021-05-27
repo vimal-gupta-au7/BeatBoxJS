0b237f6f9dcfe822522302e2001e8b8a6040e439
Vimal Gupta <vimal76869@gmail.com>
2021-05-27 09:09:15 +0530

    Completed

--- utils.js
+++ utils.js
@@ -1,28 +1,44 @@
-/**
- * Beat class that keeps track of playing the audio
- * HINT: Make sure to pass in the audioSrc as parameter to create a new audio track
- * HINT: Create a play function to play the audio if called
- */
-class Beat {}
+// Beat class that keeps track of playing the audio
+class Beat {
+    constructor(audioSrc) {
+        this.audio = new Audio(audioSrc);
+    }
+    play = () => {
+        this.audio.currentTime = 0;
+        this.audio.play();
+    };
+}
 
-/**
- * Button class that keeps track of the button color based on a press
- */
+// Button class that keeps track of the button color based on a press
 class Button {
-    constructor(color, keyCode) {}
+    constructor(color, keyCode) {
+        this.color = color;
+        this.keyCode = keyCode;
+        this.element = document.getElementById(keyCode);
+        this.setButtonColorInHTML();
+        this.setTransitionEndListener();
+    }
 
-    /**
-     * Set the button color based on color specified
-     */
-    setButtonColorInHTML = () => {};
+    // Set the button color based on color specified
+    setButtonColorInHTML = () => {
+        this.element.style.borderColor = this.color;
+    };
 
-    /**
-     * Select function to set the background color and boxShadow
-     */
-    select = () => {};
+    // Select function to set the background color and boxShadow
+    select = () => {
+        this.element.style.backgroundColor = this.color;
+        this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`;
+    };
 
-    /**
-     * Deselect function to reset background color and boxShadow
-     */
-    deselect = () => {};
+    // Deselect function to reset background color and boxShadow
+    deselect = () => {
+        this.element.style.backgroundColor = "transparent";
+        this.element.style.boxShadow = "none";
+    };
+
+    setTransitionEndListener = () => {
+        this.element.addEventListener("transitionend", () => {
+            this.deselect();
+        });
+    };
 }
